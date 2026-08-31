d73b17b9d33ddff25a3fe360d045dd048549ca32
GWW <49675917+Irish-kw@users.noreply.github.com>
2026-08-31 16:17:28 +0800

    fix: apply premerge dashboard hardening

--- src/execweave/dashboard_shell.py
+++ src/execweave/dashboard_shell.py
@@ -7,6 +7,7 @@ from .live_view import LIVE_HTML as _BASE_LIVE_HTML
 from .viewer_agent_panel import inject_agent_panel
 from .viewer_dashboard_clean import fold_budget_bootstrap, inject_live_dashboard_clean
 from .viewer_dashboard_focus import inject_live_dashboard_focus
+from .viewer_dashboard_hardening_v2 import harden_dashboard_html
 from .viewer_live_layout import inject_live_dashboard_layout
 
 
@@ -14,7 +15,8 @@ def _build_dashboard_html() -> str:
     html = inject_live_dashboard_layout(
         inject_live_dashboard_focus(inject_live_dashboard_clean(_BASE_LIVE_HTML))
     )
-    return inject_agent_panel(html)
+    html = inject_agent_panel(html)
+    return harden_dashboard_html(html)
 
 
 DASHBOARD_HTML = _build_dashboard_html()
