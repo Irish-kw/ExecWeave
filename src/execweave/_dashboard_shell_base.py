b99476fdb89bc4cec7ba448b2fb29bb3fcefd6bd
GWW <49675917+Irish-kw@users.noreply.github.com>
2026-09-08 11:39:29 +0800

    Apply PR70 dashboard projection and decross repairs

--- src/execweave/_dashboard_shell_base.py
+++ src/execweave/_dashboard_shell_base.py
@@ -6,8 +6,9 @@ from typing import Any
 from .live_view import LIVE_HTML as _BASE_LIVE_HTML
 from .viewer_agent_panel import inject_agent_panel
 from .viewer_dashboard_clean import fold_budget_bootstrap, inject_live_dashboard_clean
-from .viewer_limits import resolve_viewer_limits, viewer_limits_bootstrap
 from .viewer_dashboard_focus import inject_live_dashboard_focus
+from .viewer_dashboard_pr70 import inject_pr70_dashboard_repairs
+from .viewer_limits import resolve_viewer_limits, viewer_limits_bootstrap
 from .viewer_live_layout import inject_live_dashboard_layout
 
 
@@ -70,6 +71,7 @@ def _build_dashboard_html() -> str:
     html = _preserve_semantic_layout_constraints(html)
     html = _preserve_semantic_arrange(html)
     html = _route_ordinary_edges_from_final_positions(html)
+    html = inject_pr70_dashboard_repairs(html)
     return _align_agent_panel_topology(inject_agent_panel(_guard_compact_live_snapshot(html)))
 
 
